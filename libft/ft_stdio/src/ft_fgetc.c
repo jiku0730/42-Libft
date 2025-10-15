65cb3b4c756728558ed3f4b328a84834459471ec
jiku0730 <kei2003730@gmail.com>
2025-10-15 17:19:31 +0900

    modified: ft_fgetc.cをNorminetteに合わせました。

--- libft/ft_stdio/src/ft_fgetc.c
+++ libft/ft_stdio/src/ft_fgetc.c
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 21:01:15 by kjikuhar          #+#    #+#             */
-/*   Updated: 2025/10/15 15:25:33 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2025/10/15 17:18:54 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 int	ft_fgetc(FILE *stream)
 {
 	unsigned char	c;
+	ssize_t			bytes_read;
 
-	ssize_t bytes_read;
 	bytes_read = fread(&c, 1, 1, stream);
 	if (bytes_read == 1)
 		return (c);
